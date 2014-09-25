e29fef5e1e1834e3477a1045674d2a517d029b6c
Drewio89 <drewio89@hotmail.it>
2014-09-25 12:02:05 +0200

    Login Dinamico

--- src/com/example/beyondto/MedalFragment_Classifica.java
+++ src/com/example/beyondto/MedalFragment_Classifica.java
@@ -25,29 +25,13 @@ public class MedalFragment_Classifica extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
- 
+    	
 	    View rootView = inflater.inflate(R.layout.fragment_medal_classifica, container, false);
-          
 	    ctx=(MedalActivity)getActivity(); 
-		   
         listViewPlayers = ( ListView )rootView.findViewById(R.id.playerlist);
         mListItem = Player.getItems();
         listViewPlayers.setAdapter(new PlayerListAdapter(ctx, R.layout.playerlist, mListItem));
+        return rootView;
         
-        /*listViewPlayers.setOnClickListener(new OnClickListener() {
-
-		@Override
-		public void onClick(View arg0) {
-			// TODO Auto-generated method stub
-			String nome = ((TextView) arg0).getText().toString();
-	        // Launching new Activity on selecting single List Item
-	        Intent i = new Intent((MedalActivity)getActivity(), ProfiloGiocatore.class);
-	        // sending data to new activity
-	        i.putExtra("nome", nome);
-	        getActivity().startActivity(i);
-		}
-        });*/
-    
-    return rootView;
     }
 }
