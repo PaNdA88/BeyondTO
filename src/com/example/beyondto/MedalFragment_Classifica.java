d950b22d5e92b70caf4156afc4343bc3ed5f12ca
Drewio89 <drewio89@hotmail.it>
2014-09-25 16:33:16 +0200

    Toast Medaglie

--- src/com/example/beyondto/MedalFragment_Classifica.java
+++ src/com/example/beyondto/MedalFragment_Classifica.java
@@ -1,20 +1,14 @@
 package com.example.beyondto;
 
 import java.util.ArrayList;
-import java.util.List;
 import com.example.beyondto.R;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.content.Context;
-import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
  
 public class MedalFragment_Classifica extends Fragment{
  
@@ -25,13 +19,11 @@ public class MedalFragment_Classifica extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-    	
 	    View rootView = inflater.inflate(R.layout.fragment_medal_classifica, container, false);
 	    ctx=(MedalActivity)getActivity(); 
         listViewPlayers = ( ListView )rootView.findViewById(R.id.playerlist);
         mListItem = Player.getItems();
         listViewPlayers.setAdapter(new PlayerListAdapter(ctx, R.layout.playerlist, mListItem));
         return rootView;
-        
     }
 }
