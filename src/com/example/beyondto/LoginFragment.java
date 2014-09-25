53f40295d1c3864a93495e37ad4506b944b9063a
Virginia Daniele <virginia.daniele@studenti.polito.it>
2014-09-25 16:51:59 +0200

    listener sulla classifica e progressbar arancione

--- src/com/example/beyondto/LoginFragment.java
+++ src/com/example/beyondto/LoginFragment.java
@@ -24,32 +24,34 @@ import com.facebook.model.GraphUser;
 import com.facebook.widget.LoginButton;
 
 public class LoginFragment extends Fragment {
-	
+
 	private static final String TAG = "MainFragment";
 	private UiLifecycleHelper uiHelper;
 	private String USER_TOKEN, EXPIRATION;
 	private Session session;
 	private boolean logSession, newUser;
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		newUser = false;
 		logSession = false;
-		Session session = new Session(((LoginActivity)getActivity()).getApplicationContext());
-	    Session.setActiveSession(session);
-	    
-	    if(session != null && 
-				(session.getState().equals(SessionState.CREATED_TOKEN_LOADED) ||	
-				session.getState().equals(SessionState.OPENING) || 
-				session.getState().equals(SessionState.OPENED_TOKEN_UPDATED))){
-	    	
-	    	logSession = true;
-	    }else{
-	    	logSession = false;
-	    }
+		Session session = new Session(
+				((LoginActivity) getActivity()).getApplicationContext());
+		Session.setActiveSession(session);
 
-        uiHelper = new UiLifecycleHelper(getActivity(), statusCallback);
+		if (session != null
+				&& (session.getState()
+						.equals(SessionState.CREATED_TOKEN_LOADED)
+						|| session.getState().equals(SessionState.OPENING) || session
+						.getState().equals(SessionState.OPENED_TOKEN_UPDATED))) {
+
+			logSession = true;
+		} else {
+			logSession = false;
+		}
+
+		uiHelper = new UiLifecycleHelper(getActivity(), statusCallback);
 		uiHelper.onCreate(savedInstanceState);
 	}
 
@@ -57,48 +59,59 @@ public class LoginFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.login_fragment, container, false);
-		
-		if(logSession){
+
+		if (logSession) {
 
 			String testo1 = "Attendere prego";
-        	TextView textTitle = (TextView) view.findViewById(R.id.titoloLogin);
-    		Resources res1 = getActivity().getResources();
-    		String tit = String.format(res1.getString(R.string.titoloLogin),testo1);
-    		textTitle.setText(Html.fromHtml((String)  tit ));
-    		
-    	    ProgressBar p = new ProgressBar((LoginActivity)getActivity(), null, android.R.attr.progressBarStyle);
-    	    p.setVisibility(View.VISIBLE);
-    	    RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(100,100);
-    	    params.addRule(RelativeLayout.CENTER_IN_PARENT);
-    	    ((ViewGroup) view).addView(p, params);
-    		
-    		String testo2 = "Stiamo effettuando la connessione a Facebook";
-        	TextView textSubtitle = (TextView) view.findViewById(R.id.sottotitolo);
-    		Resources res2 = getActivity().getResources();
-    		String sott = String.format(res2.getString(R.string.sottotitolo),testo2);
-    		textSubtitle.setText(Html.fromHtml((String)  sott ));
-				
+			TextView textTitle = (TextView) view.findViewById(R.id.titoloLogin);
+			Resources res1 = getActivity().getResources();
+			String tit = String.format(res1.getString(R.string.titoloLogin),
+					testo1);
+			textTitle.setText(Html.fromHtml((String) tit));
+
+			ProgressBar p = new ProgressBar((LoginActivity) getActivity(),
+					null, android.R.attr.progressBarStyle);
+			p.getIndeterminateDrawable().setColorFilter(
+					getResources().getColor(R.color.accent_orange),
+					android.graphics.PorterDuff.Mode.SRC_IN);
+			p.setVisibility(View.VISIBLE);
+			RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+					100, 100);
+			params.addRule(RelativeLayout.CENTER_IN_PARENT);
+			((ViewGroup) view).addView(p, params);
+
+			String testo2 = "Stiamo effettuando la connessione a Facebook";
+			TextView textSubtitle = (TextView) view
+					.findViewById(R.id.sottotitolo);
+			Resources res2 = getActivity().getResources();
+			String sott = String.format(res2.getString(R.string.sottotitolo),
+					testo2);
+			textSubtitle.setText(Html.fromHtml((String) sott));
+
 		}
-				
-		else{
-			
+
+		else {
+
 			String testo1 = "ACCEDI A FACEBOOK!";
-        	TextView textTitle = (TextView) view.findViewById(R.id.titoloLogin);
-    		Resources res1 = getActivity().getResources();
-    		String tit = String.format(res1.getString(R.string.titoloLogin),testo1);
-    		textTitle.setText(Html.fromHtml((String)  tit ));
-    		
-    		String testo2 = "Per giocare con BeyondTO è necessario effettuare l'accesso a Facebook";
-        	TextView textSubtitle = (TextView) view.findViewById(R.id.sottotitolo);
-    		Resources res2 = getActivity().getResources();
-    		String sott = String.format(res2.getString(R.string.sottotitolo),testo2);
-    		textSubtitle.setText(Html.fromHtml((String)  sott ));	
+			TextView textTitle = (TextView) view.findViewById(R.id.titoloLogin);
+			Resources res1 = getActivity().getResources();
+			String tit = String.format(res1.getString(R.string.titoloLogin),
+					testo1);
+			textTitle.setText(Html.fromHtml((String) tit));
+
+			String testo2 = "Per giocare con BeyondTO è necessario effettuare l'accesso a Facebook";
+			TextView textSubtitle = (TextView) view
+					.findViewById(R.id.sottotitolo);
+			Resources res2 = getActivity().getResources();
+			String sott = String.format(res2.getString(R.string.sottotitolo),
+					testo2);
+			textSubtitle.setText(Html.fromHtml((String) sott));
 		}
-        
+
 		LoginButton authButton = (LoginButton) view
 				.findViewById(R.id.authButton);
-		// richiedo i permessi 
-		authButton.setReadPermissions(Arrays.asList("public_profile","email"));
+		// richiedo i permessi
+		authButton.setReadPermissions(Arrays.asList("public_profile", "email"));
 		authButton.setFragment(this);
 		return view;
 	}
@@ -118,66 +131,70 @@ public class LoginFragment extends Fragment {
 			getUserToken(session);
 			Log.d("USER TOKEN:", USER_TOKEN);
 			Log.d("TOKEN EXPIRE:", EXPIRATION);
-			
+
 		} else if (state.isClosed()) {
 			Log.i(TAG, "Logged out...");
 		}
 	}
 
 	// mi ricavo il token creato da facebook
-	public void getUserToken(final Session session){  
-		USER_TOKEN = session.getAccessToken().toString(); 
-		EXPIRATION = (session.getExpirationDate()).toString(); 
-		
-		Request request = Request.newMeRequest(session, 
-	            new Request.GraphUserCallback() {
-	        private boolean newUser;
+	public void getUserToken(final Session session) {
+		USER_TOKEN = session.getAccessToken().toString();
+		EXPIRATION = (session.getExpirationDate()).toString();
 
-			@Override
-	        public void onCompleted(GraphUser user, Response response) {
-	            if (session == Session.getActiveSession()) {
-	                if (user != null) {	                	
-	                	
-	                    String USER_ID = user.getId();
-	                    Log.d("USER ID", USER_ID);
-	                    String USER_NAME = user.getName();
-	                    Log.d("USER NAME",USER_NAME);
-	                    String USER_EMAIL = user.asMap().get("email").toString();
-	                    Log.d("USER EMAIL",USER_EMAIL);
-	            		Connector con = new Connector();
-	            		String result = con.doLoginFromFacebook(USER_ID, USER_TOKEN, EXPIRATION,USER_EMAIL, USER_NAME );
-	            		Log.d("RISULTATO:",result);
-	            		if(result.equals("0")){
-	            			newUser = true;
-	            			//goToChoiseClan(); 
-	            		}
-	            		/*else{
-	            			goToTorinoHome();  
-	            		}*/
-	            		goToTorinoHome(); 
-	                }
-	            }
-	            if (response.getError() != null) {
-	            	
-	            	Log.d("ERROR:", response.getError().toString());
-	            }
-	        }
-	    });
-	    request.executeAsync();		
-	} 
-	
-	public void goToTorinoHome(){
-		Intent i = new Intent((LoginActivity)getActivity(), HomeActivity.class);
+		Request request = Request.newMeRequest(session,
+				new Request.GraphUserCallback() {
+					private boolean newUser;
+
+					@Override
+					public void onCompleted(GraphUser user, Response response) {
+						if (session == Session.getActiveSession()) {
+							if (user != null) {
+
+								String USER_ID = user.getId();
+								Log.d("USER ID", USER_ID);
+								String USER_NAME = user.getName();
+								Log.d("USER NAME", USER_NAME);
+								String USER_EMAIL = user.asMap().get("email")
+										.toString();
+								Log.d("USER EMAIL", USER_EMAIL);
+								Connector con = new Connector();
+								String result = con.doLoginFromFacebook(
+										USER_ID, USER_TOKEN, EXPIRATION,
+										USER_EMAIL, USER_NAME);
+								Log.d("RISULTATO:", result);
+								if (result.equals("0")) {
+									newUser = true;
+									// goToChoiseClan();
+								}
+								/*
+								 * else{ goToTorinoHome(); }
+								 */
+								goToTorinoHome();
+							}
+						}
+						if (response.getError() != null) {
+
+							Log.d("ERROR:", response.getError().toString());
+						}
+					}
+				});
+		request.executeAsync();
+	}
+
+	public void goToTorinoHome() {
+		Intent i = new Intent((LoginActivity) getActivity(), HomeActivity.class);
 		i.putExtra("session", session);
-		getActivity().startActivity(i); 
+		getActivity().startActivity(i);
 	}
-	
-	public void goToChoiseClan(){
-		Intent i = new Intent((LoginActivity)getActivity(),ChoiseClanActivity.class);
+
+	public void goToChoiseClan() {
+		Intent i = new Intent((LoginActivity) getActivity(),
+				ChoiseClanActivity.class);
 		i.putExtra("tokenUser", USER_TOKEN);
-		getActivity().startActivity(i); 
+		getActivity().startActivity(i);
 	}
-	
+
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
--- src/com/example/beyondto/MedalFragment_Classifica.java
+++ src/com/example/beyondto/MedalFragment_Classifica.java
@@ -5,25 +5,43 @@ import com.example.beyondto.R;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
- 
-public class MedalFragment_Classifica extends Fragment{
- 
+
+public class MedalFragment_Classifica extends Fragment implements
+		OnItemClickListener {
+
 	private ListView listViewPlayers;
 	private ArrayList mListItem;
 	private Context ctx;
-   
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-	    View rootView = inflater.inflate(R.layout.fragment_medal_classifica, container, false);
-	    ctx=(MedalActivity)getActivity(); 
-        listViewPlayers = ( ListView )rootView.findViewById(R.id.playerlist);
-        mListItem = Player.getItems();
-        listViewPlayers.setAdapter(new PlayerListAdapter(ctx, R.layout.playerlist, mListItem));
-        return rootView;
-    }
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_medal_classifica,
+				container, false);
+		ctx = (MedalActivity) getActivity();
+		listViewPlayers = (ListView) rootView.findViewById(R.id.playerlist);
+		mListItem = Player.getItems();
+		listViewPlayers.setAdapter(new PlayerListAdapter(ctx,
+				R.layout.playerlist, mListItem));
+		listViewPlayers.setOnItemClickListener(this);
+		return rootView;
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) {
+
+		Intent intent = new Intent();
+		intent.setClass(getActivity(), SettingsActivity.class);
+
+		startActivity(intent);
+
+	}
 }
