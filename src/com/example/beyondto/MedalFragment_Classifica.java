cc276380557cdef9f486e2ed98bbc1cd81f2b3bf
Drewio89 <drewio89@hotmail.it>
2014-09-28 11:11:53 +0200

    Commit classifica non ancora finita
    (solo perchè altrimenti non posso fare il pull delle cose di Anto)

--- src/com/example/beyondto/MedalFragment_Classifica.java
+++ src/com/example/beyondto/MedalFragment_Classifica.java
@@ -38,10 +38,14 @@ public class MedalFragment_Classifica extends Fragment implements
 	public void onItemClick(AdapterView<?> parent, View view, int position,
 			long id) {
 
-		String s = "ciccia";
+		String nome = "Andre M";
+		//ImageView imgClan;
+		String punti = "4563";
 		Intent intent = new Intent();
 		intent.setClass(getActivity(), ProfiloGiocatore.class);
-		intent.putExtra("nome", s);
+		intent.putExtra("nome", nome);
+		intent.putExtra("punti", punti);
+		//intent.putExtra("clan", imgClan);
 		startActivity(intent);
 
 	}
